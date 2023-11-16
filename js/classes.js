2a2d2c4a2c963149241432f745457010d41259cb
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2023-11-16 14:00:45 -0300

    Criando jogo de luta (POO)

---
+++ js/classes.js
@@ -0,0 +1,116 @@
+//Criando as classes do jogo
+
+class Character {
+    _life = 1;
+    maxLife = 100;
+    attack = 0;
+    defense = 0;
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    get life() {
+        return this._life;
+    }
+    set life(newLife) {
+        this._life = newLife < 0 ? 0 : newLife;
+    }
+};
+
+class Knight extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 100;
+        this.attack = 10;
+        this.defense = 8;
+        this.maxLife = this.life;
+    }
+}
+
+class Sorcecer extends Character {
+    constructor(name) {
+        super(name);
+        this.life = 80;
+        this.attack = 15;
+        this.defense = 3;
+        this.maxLife = this.life;
+    }
+}
+
+
+class LittleMonster extends Character {
+    constructor() {
+        super("Little Monster");
+        this.life = 40;
+        this.attack = 4;
+        this.defense = 4;
+        this.maxLife = this.life;
+    }
+}
+
+class BigMonster extends Character {
+    constructor() {
+        super("Big Monster");
+        this.life = 120;
+        this.attack = 16;
+        this.defense = 6;
+        this.maxLife = this.life;
+    }
+}
+
+class Stage {
+    constructor(fighter1, fighter2, fighter1El, fighter2El) {
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+        this.fighter1El = fighter1El;
+        this.fighter2El = fighter2El;
+    }
+
+    start() {
+        this.update();
+
+        this.fighter1El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
+        this.fighter2El.querySelector('.attackButton').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
+        
+    }
+
+    update() {
+        //FIGHTER 1
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)} HP`;
+        //pegando e preenchendo a vida do fighter1
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
+        this.fighter1El.querySelector('.lifebar .bar').style.width = `${f1Pct}%`;
+
+
+
+        //FIGHTER 2
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`;
+        //pegando e preenchendo a vida do fighter2
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+        this.fighter2El.querySelector('.lifebar .bar').style.width = `${f2Pct}%`;
+    }
+
+    doAttack(attacking, attacked){
+
+        if (attacking.life <= 0 || attacked.life <= 0){
+            console.log("Não é possível atacar um personagem morto");
+            return;
+        }
+
+        let attackFactor = (Math.random() * 2).toFixed(2);
+        let defenseFactor = (Math.random() * 2).toFixed(2);
+
+        let realAttack = attackFactor * attacking.attack;
+        let realDefense = defenseFactor * attacked.defense;
+
+        if (realAttack > realDefense){
+            attacked.life -= realAttack;
+        } else {
+            console.log("Ataque defendido");
+        }
+
+
+        this.update();
+    }
+}
